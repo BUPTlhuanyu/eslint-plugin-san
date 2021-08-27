752cad4ee8907906451a5c91e2c8866bd8d0b4bc
BUPTlhuanyu <liaohuanyutot@163.com>
2021-08-27 20:31:35 +0800

    feat: 检测双向绑定 fixes #16

---
+++ __tests__/lib/rules/valid-two-way-bind.test.js
@@ -0,0 +1,155 @@
+/**
+ * @file
+ */
+
+/**
+ * @author Toru Nagashima
+ * @copyright 2017 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+ 'use strict';
+
+ // ------------------------------------------------------------------------------
+ // Requirements
+ // ------------------------------------------------------------------------------
+ 
+ const RuleTester = require('eslint').RuleTester;
+ const rule = require('../../../lib/rules/valid-template-root');
+ 
+ // ------------------------------------------------------------------------------
+ // Tests
+ // ------------------------------------------------------------------------------
+ 
+ const tester = new RuleTester({
+     parser: require.resolve('san-eslint-parser'),
+     parserOptions: {ecmaVersion: 2015}
+ });
+ 
+ tester.run('valid-template-root', rule, {
+     valid: [
+         {
+             filename: 'test.san',
+             code: `
+                <div>
+                <p a="{=!isOK=}"></p>
+                <p a="{=!!isOK=}"></p>
+                <p a="{=-num1 + num2=}"></p>
+                <p a="{=num1 + num2=}"></p>
+                <p a="{=num1 - num2=}"></p>
+                <p a="{=num1 * num2=}"></p>
+                <p a="{=num1 / num2=}"></p>
+                <p a="{=num1 + num2 * num3=}"></p>
+                <p a="{=num1 > num2=}"></p>
+                <p a="{=num1 !== num2=}"></p>
+                <p a="{=num1 > num2 ? num1 : num2=}"></p>
+                <p a="{=a * (b + c)=}"></p>
+                <p a="{=num1 + 200=}"></p>
+                <p a="{=item ? ',' + item : ''=}"></p>
+                <x-list data="{{ persons || [] =}"/>
+                <x-article data="{= {title: articleTitle, author: user} =}" />
+                <p a="{=max(num1, num2)=}"></p>
+                </div>
+             `
+         },
+         {
+             filename: 'test.san',
+             code: '<template><div>abc</div></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template>\n    <div>abc</div>\n</template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template>\n    <!-- comment -->\n    <div>abc</div>\n</template>'
+         },
+         {
+             filename: 'test.san',
+             code:
+                 '<template>\n    <!-- comment -->\n    <div s-if="foo">abc</div>\n    <div s-else>abc</div>\n</template>'
+         },
+         {
+             filename: 'test.san',
+             code:
+                 '<template>\n    <!-- comment -->\n    <div s-if="foo">abc</div>\n    <div s-else-if="bar">abc</div>\n    <div s-else>abc</div>\n</template>'
+         },
+         {
+             filename: 'test.san',
+             code: `<template>\n    <c1 s-if="1" />\n    <c2 s-else-if="1" />\n    <c3 s-else />\n</template>`
+         },
+         {
+             filename: 'test.san',
+             code: '<template><div s-if="foo"></div></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><div s-if="foo"></div><div s-else-if="bar"></div></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template src="foo.html"></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><div><textarea/>test</div></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><table><custom-thead></custom-thead></table></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template lang="pug">test</template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><div></div><div></div></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template>\n    <div></div>\n    <div></div>\n</template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template>{{a b c}}</template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><div></div>aaaaaa</template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template>aaaaaa<div></div></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><div s-for="x in list"></div></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><slot></slot></template>'
+         },
+         {
+             filename: 'test.san',
+             code: '<template><template></template></template>'
+         }
+     ],
+     invalid: [
+         {
+             filename: 'test.san',
+             code: '<template>\n</template>',
+             errors: ['The template requires child element.']
+         },
+         {
+             filename: 'test.san',
+             code: '<template src="foo.html">abc</template>',
+             errors: ["The template root with 'src' attribute is required to be empty."]
+         },
+         {
+             filename: 'test.san',
+             code: '<template src="foo.html"><div></div></template>',
+             errors: ["The template root with 'src' attribute is required to be empty."]
+         }
+     ]
+ });
+ 
